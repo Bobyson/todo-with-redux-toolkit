882f32f4bc09a04cb4885fb23401ca938035d6e8
Bobyson Laishram <acntanouba@gmail.com>
2025-02-11 21:40:08 +0530

    initialize Redux Toolkit with Todo functionality and integrate Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import AddTodo from './components/AddTodo'
+import TodoItem from './components/TodoItem'
+
+function App() {
+
+  return (
+    <>
+      <AddTodo />
+      <TodoItem />
+    </>
+  )
+}
+
+export default App
